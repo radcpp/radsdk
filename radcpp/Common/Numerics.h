81baf62c32bfdea0a7400ff82af756e51d0336c9
Jerry Ma <majunlichn@outlook.com>
2022-04-13 00:21:55 +0800

    radcpp/Common: add Numerics.h

---
+++ radcpp/Common/Numerics.h
@@ -0,0 +1,27 @@
+#ifndef RADCPP_NUMERICS_H
+#define RADCPP_NUMERICS_H
+#pragma once
+
+#include "radcpp/Common/Common.h"
+#include "radcpp/Common/Math.h"
+
+#include <numbers>
+#include <complex>
+#include <valarray>
+
+// Numeric algorithms
+#include <numeric>
+
+// Misc
+#include <random>
+#include <ratio>
+#include <cfenv>
+#include <bit>
+
+// Boost.Multiprecision
+// Please refer to: https://www.boost.org/doc/libs/1_78_0/libs/multiprecision/doc/html/index.html
+#include <boost/multiprecision/cpp_int.hpp>
+#include <boost/multiprecision/cpp_bin_float.hpp>
+#include <boost/multiprecision/cpp_dec_float.hpp>
+
+#endif // RADCPP_NUMERICS_H
