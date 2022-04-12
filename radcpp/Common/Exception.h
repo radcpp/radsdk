f5572e884153024db6c635e1f1d5375d6a7665ed
Jerry Ma <majunlichn@outlook.com>
2022-04-13 00:20:01 +0800

    radcpp/Common: add Exception.h

---
+++ radcpp/Common/Exception.h
@@ -0,0 +1,11 @@
+#ifndef RADCPP_EXCEPTION_H
+#define RADCPP_EXCEPTION_H
+#pragma once
+
+#include <cassert>
+#include <cerrno>
+#include <exception>
+#include <stdexcept>
+#include <system_error>
+
+#endif // RADCPP_EXCEPTION_H
