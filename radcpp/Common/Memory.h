118056472754843b902b2b0ea2d1be2e9a703f0b
Jerry Ma <majunlichn@outlook.com>
2022-04-13 19:55:34 +0800

    radcpp/Common: add Memory.h

---
+++ radcpp/Common/Memory.h
@@ -0,0 +1,11 @@
+#ifndef RADCPP_MEMORY_H
+#define RADCPP_MEMORY_H
+#pragma once
+
+#include "radcpp/Common/Common.h"
+#include <memory>
+
+#include <boost/align/aligned_allocator.hpp>
+#include <boost/smart_ptr.hpp>
+
+#endif // RADCPP_MEMORY_H
