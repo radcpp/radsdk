ae746f2ff8d6497193fe18baf4202cba6ec187a6
Jerry Ma <majunlichn@outlook.com>
2022-05-02 13:12:04 +0800

    radcpp/Common/Memory: RefCounted
    Intrusive reference counted.

--- radcpp/Common/Memory.h
+++ radcpp/Common/Memory.h
@@ -8,380 +8,290 @@
 #include <boost/align/aligned_allocator.hpp>
 #include <boost/smart_ptr.hpp>
 
-// An Intrusive Smart Pointer
-// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2018/p0468r1.html
-// https://github.com/bruxisma/retain-ptr
+template<class T>
+class RefCounted
+{
+protected:
+    RefCounted()
+    {
+    }
+    ~RefCounted()
+    {
+    }
+    size_t GetUseCount()
+    {
+        return m_count;
+    }
+private:
+    mutable std::atomic<size_t> m_count{ 0 };
 
-namespace sg14 {
-
-    using std::is_convertible;
-    using std::is_same;
-
-    using std::conditional_t;
-    using std::add_pointer_t;
-    using std::nullptr_t;
-
-    using std::declval;
-
-} /* namespace sg14 */
-
-namespace sg14 {
-    namespace impl {
-
-        template <class T> struct identity { using type = T; };
-
-        template <class T, class Void, template <class...> class, class...>
-        struct detector : identity<T> { using value_t = std::false_type; };
-
-        template <class T, template <class...> class U, class... Args>
-        struct detector<T, std::void_t<U<Args...>>, U, Args...> :
-            identity<U<Args...>>
+    friend void AddRef(const T* ptr)
+    {
+        ptr->m_count.fetch_add(1, std::memory_order_relaxed);
+    }
+    friend void Release(const T* ptr)
+    {
+        if (ptr->m_count.fetch_sub(1, std::memory_order_release) == 1)
         {
-            using value_t = std::true_type;
-        };
-
-        struct nonesuch final {
-            nonesuch(nonesuch const&) = delete;
-            nonesuch() = delete;
-            ~nonesuch() = delete;
-
-            void operator = (nonesuch const&) = delete;
-        };
-
-        template <class T, template <class...> class U, class... Args>
-        using detected_or = detector<T, void, U, Args...>;
-
-        template <template <class...> class T, class... Args>
-        using detected_t = typename detected_or<nonesuch, T, Args...>::type;
-
-        template <class T, template <class...> class U, class... Args>
-        using detected_or_t = typename detected_or<T, U, Args...>::type;
-
-        template <template <class...> class T, class... Args>
-        using is_detected = typename detected_or<nonesuch, T, Args...>::value_t;
-
-        template <class To, template <class...> class T, class... Args>
-        using is_detected_convertible = std::is_convertible<
-            detected_t<T, Args...>,
-            To
-        >;
-
-        template <class T, template <class...> class U, class... Args>
-        using is_detected_exact = std::is_same<T, detected_t<U, Args...>>;
-
-        template <class T>
-        using has_pointer = typename T::pointer;
-
-        template <class T>
-        using has_default_action = typename T::default_action;
-
-        template <class T, class P>
-        using has_use_count = decltype(T::use_count(std::declval<P>()));
-
+            std::atomic_thread_fence(std::memory_order_acquire);
+            delete ptr;
+        }
     }
-} /* namespace sg14::impl */
+};
 
-namespace sg14 {
-
-    using impl::detected_or_t;
-    using impl::is_detected;
-
-    template <class> struct retain_traits;
-
-    template <class T>
-    class atomic_reference_count {
-        template <class> friend struct retain_traits;
-    protected:
-        atomic_reference_count() = default;
-    private:
-        std::atomic<long> count{ 1 };
-    };
-
-    template <class T>
-    class reference_count {
-        template <class> friend struct retain_traits;
-    protected:
-        reference_count() = default;
-    private:
-        long count{ 1 };
-    };
-
-    struct retain_object_t { retain_object_t() noexcept = default; };
-    struct adopt_object_t { adopt_object_t() noexcept = default; };
-
-    constexpr retain_object_t retain_object{ };
-    constexpr adopt_object_t adopt_object{ };
-
-    template <class T>
-    struct retain_traits final {
-    public:
-        template <class U>
-        using enable_if_base = std::enable_if_t<std::is_base_of_v<U, T>>;
-
-        template <class U, class = enable_if_base<U>>
-        static void increment(atomic_reference_count<U>* ptr) noexcept {
-            ptr->count.fetch_add(1, std::memory_order_relaxed);
-        }
-
-        template <class U, class = enable_if_base<U>>
-        static void decrement(atomic_reference_count<U>* ptr) noexcept {
-            ptr->count.fetch_sub(1, std::memory_order_acq_rel);
-            if (not use_count(ptr)) { delete static_cast<T*>(ptr); }
-        }
-
-        template <class U, class = enable_if_base<U>>
-        static long use_count(atomic_reference_count<U>* ptr) noexcept {
-            return ptr->count.load(std::memory_order_relaxed);
-        }
-
-        template <class U, class = enable_if_base<U>>
-        static void increment(reference_count<U>* ptr) noexcept {
-            ++ptr->count;
-        }
-        template <class U, class = enable_if_base<U>>
-        static void decrement(reference_count<U>* ptr) noexcept {
-            --ptr->count;
-            if (not use_count(ptr)) { delete static_cast<T*>(ptr); }
-        }
-        template <class U, class = enable_if_base<U>>
-        static long use_count(reference_count<U>* ptr) noexcept {
-            return ptr->count;
-        }
-    };
-
-    template <class T, class R = retain_traits<T>>
-    class retain_ptr {
-    public:
-        using element_type = T;
-        using traits_type = R;
-
-        using pointer = detected_or_t<
-            add_pointer_t<element_type>,
-            impl::has_pointer,
-            traits_type
-        >;
-
-        using default_action = detected_or_t<
-            adopt_object_t,
-            impl::has_default_action,
-            traits_type
-        >;
-
-        static constexpr bool CheckAction = std::disjunction_v<
-            std::is_same<default_action, adopt_object_t>,
-            std::is_same<default_action, retain_object_t>
-        >;
-
-        static_assert(
-            CheckAction,
-            "traits_type::default_action must be adopt_object_t or retain_object_t");
-
-        static constexpr auto has_use_count = is_detected<
-            impl::has_use_count,
-            traits_type,
-            pointer
-        >{ };
-
-        retain_ptr(pointer ptr, retain_object_t) :
-            retain_ptr{ ptr, adopt_object }
+template<class T>
+class Ref
+{
+public:
+    constexpr Ref() noexcept :
+        m_ptr(nullptr)
+    {}
+    Ref(T* p, bool addref = true) :
+        m_ptr(p)
+    {
+        if (m_ptr && addref)
         {
-            if (*this) { traits_type::increment(this->get()); }
+            AddRef(m_ptr);
         }
-
-        retain_ptr(pointer ptr, adopt_object_t) : m_ptr{ ptr } { }
-
-        explicit retain_ptr(pointer ptr) :
-            retain_ptr{ ptr, default_action() }
-        { }
-
-        retain_ptr(nullptr_t) : retain_ptr{ } { }
-
-        retain_ptr(retain_ptr const& that) :
-            m_ptr{ that.m_ptr }
+    }
+    Ref(Ref const& rhs) :
+        m_ptr(rhs.get())
+    {
+        if (m_ptr)
         {
-            if (*this) { traits_type::increment(this->get()); }
+            AddRef(m_ptr);
         }
-
-        retain_ptr(retain_ptr&& that) noexcept :
-            m_ptr{ that.detach() }
-        { }
-
-        retain_ptr() noexcept = default;
-        ~retain_ptr() {
-            if (*this) { traits_type::decrement(this->get()); }
+    }
+    template<class U>
+    Ref(Ref<U> const& rhs) :
+        m_ptr(rhs.get())
+    {
+        if (m_ptr)
+        {
+            AddRef(m_ptr);
         }
+    }
 
-        retain_ptr& operator = (retain_ptr const& that) {
-            retain_ptr(that).swap(*this);
-            return *this;
+    ~Ref()
+    {
+        if (m_ptr)
+        {
+            Release(m_ptr);
         }
-
-        retain_ptr& operator = (retain_ptr&& that) {
-            retain_ptr(std::move(that)).swap(*this);
-            return *this;
-        }
-
-        retain_ptr& operator = (nullptr_t) noexcept { this->reset(); return *this; }
-
-        void swap(retain_ptr& that) noexcept {
-            using std::swap;
-            swap(this->m_ptr, that.m_ptr);
-        }
-
-        explicit operator bool() const noexcept { return this->get(); }
-        decltype(auto) operator * () const noexcept { return *this->get(); }
-        pointer operator -> () const noexcept { return this->get(); }
-
-        pointer get() const noexcept { return this->m_ptr; }
-
-        long use_count() const {
-            if constexpr (has_use_count) {
-                return this->get() ? traits_type::use_count(this->get()) : 0;
-            }
-            else { return -1; }
-        }
-
-        pointer detach() noexcept {
-            auto ptr = this->get();
-            this->m_ptr = pointer{ };
-            return ptr;
-        }
-
-        void reset(pointer ptr, retain_object_t) {
-            *this = retain_ptr(ptr, retain_object);
-        }
-
-        void reset(pointer ptr, adopt_object_t) noexcept {
-            *this = retain_ptr(ptr, adopt_object);
-        }
-
-        void reset(pointer ptr) { *this = retain_ptr(ptr, default_action()); }
-
-    private:
-        pointer m_ptr{ };
-    };
-
-    template <class T, class R>
-    void swap(retain_ptr<T, R>& lhs, retain_ptr<T, R>& rhs) noexcept {
-        lhs.swap(rhs);
     }
 
-    template <class T, class R>
-    bool operator == (
-        retain_ptr<T, R> const& lhs,
-        retain_ptr<T, R> const& rhs
-        ) noexcept {
-        return lhs.get() == rhs.get();
+    template<class U>
+    Ref& operator=(Ref<U> const& rhs)
+    {
+        Ref(rhs).swap(*this);
+        return *this;
+    }
+    Ref& operator=(std::nullptr_t) noexcept
+    {
+        reset();
+        return *this;
     }
 
-    template <class T, class R>
-    bool operator != (
-        retain_ptr<T, R> const& lhs,
-        retain_ptr<T, R> const& rhs
-        ) noexcept {
-        return lhs.get() != rhs.get();
+    Ref(Ref&& rhs) noexcept :
+        m_ptr(rhs.m_ptr)
+    {
+        rhs.m_ptr = nullptr;
     }
 
-    template <class T, class R>
-    bool operator >= (
-        retain_ptr<T, R> const& lhs,
-        retain_ptr<T, R> const& rhs
-        ) noexcept {
-        return lhs.get() >= rhs.get();
+    Ref& operator=(Ref&& rhs) noexcept
+    {
+        Ref(static_cast<Ref&&>(rhs)).swap(*this);
+        return *this;
     }
 
-    template <class T, class R>
-    bool operator <= (
-        retain_ptr<T, R> const& lhs,
-        retain_ptr<T, R> const& rhs
-        ) noexcept {
-        return lhs.get() <= rhs.get();
+    template<class U> friend class Ref;
+    template<class U>
+    Ref(Ref<U>&& rhs) :
+        m_ptr(rhs.m_ptr)
+    {
+        rhs.m_ptr = nullptr;
+    }
+    template<class U>
+    Ref& operator=(Ref<U>&& rhs) noexcept
+    {
+        Ref(static_cast<Ref<U>&&>(rhs)).swap(*this);
+        return *this;
     }
 
-    template <class T, class R>
-    bool operator > (
-        retain_ptr<T, R> const& lhs,
-        retain_ptr<T, R> const& rhs
-        ) noexcept {
-        return lhs.get() > rhs.get();
+    Ref& operator=(Ref const& rhs)
+    {
+        Ref(rhs).swap(*this);
+        return *this;
+    }
+    Ref& operator=(T* rhs)
+    {
+        Ref(rhs).swap(*this);
+        return *this;
     }
 
-    template <class T, class R>
-    bool operator < (
-        retain_ptr<T, R> const& lhs,
-        retain_ptr<T, R> const& rhs
-        ) noexcept {
-        return lhs.get() < rhs.get();
+    void reset() noexcept
+    {
+        Ref().swap(*this);
     }
 
-    template <class T, class R>
-    bool operator == (retain_ptr<T, R> const& lhs, nullptr_t) noexcept {
-        return bool(lhs);
+    void reset(T* rhs) noexcept
+    {
+        Ref(rhs).swap(*this);
     }
 
-    template <class T, class R>
-    bool operator != (retain_ptr<T, R> const& lhs, nullptr_t) noexcept {
-        return not lhs;
+    void reset(T* rhs, bool addref) noexcept
+    {
+        Ref(rhs, addref).swap(*this);
     }
 
-    template <class T, class R>
-    bool operator >= (retain_ptr<T, R> const& lhs, nullptr_t) noexcept {
-        return not (lhs < nullptr);
+    T* get() const noexcept
+    {
+        return m_ptr;
     }
 
-    template <class T, class R>
-    bool operator <= (retain_ptr<T, R> const& lhs, nullptr_t) noexcept {
-        return nullptr < lhs;
+    T* detach() noexcept
+    {
+        T* p = m_ptr;
+        m_ptr = 0;
+        return p;
     }
 
-    template <class T, class R>
-    bool operator > (retain_ptr<T, R> const& lhs, nullptr_t) noexcept {
-        return not (nullptr < lhs);
+    T& operator*() const noexcept
+    {
+        assert(m_ptr != 0);
+        return *m_ptr;
     }
 
-    template <class T, class R>
-    bool operator < (retain_ptr<T, R> const& lhs, nullptr_t) noexcept {
-        return std::less<>()(lhs.get(), nullptr);
+    T* operator->() const noexcept
+    {
+        assert(m_ptr != 0);
+        return m_ptr;
     }
 
-    template <class T, class R>
-    bool operator == (nullptr_t, retain_ptr<T, R> const& rhs) noexcept {
-        return not rhs;
+    void swap(Ref& rhs) noexcept
+    {
+        std::swap(m_ptr, rhs.m_ptr);
     }
 
-    template <class T, class R>
-    bool operator != (nullptr_t, retain_ptr<T, R> const& rhs) noexcept {
-        return bool(rhs);
+private:
+    T* m_ptr;
+};
+
+template<class T, class U> inline bool operator==(Ref<T> const& a, Ref<U> const& b) noexcept
+{
+    return a.get() == b.get();
+}
+
+template<class T, class U> inline bool operator!=(Ref<T> const& a, Ref<U> const& b) noexcept
+{
+    return a.get() != b.get();
+}
+
+template<class T, class U> inline bool operator==(Ref<T> const& a, U* b) noexcept
+{
+    return a.get() == b;
+}
+
+template<class T, class U> inline bool operator!=(Ref<T> const& a, U* b) noexcept
+{
+    return a.get() != b;
+}
+
+template<class T, class U> inline bool operator==(T* a, Ref<U> const& b) noexcept
+{
+    return a == b.get();
+}
+
+template<class T, class U> inline bool operator!=(T* a, Ref<U> const& b) noexcept
+{
+    return a != b.get();
+}
+
+template<class T> inline bool operator==(Ref<T> const& p, std::nullptr_t) noexcept
+{
+    return p.get() == 0;
+}
+
+template<class T> inline bool operator==(std::nullptr_t, Ref<T> const& p) noexcept
+{
+    return p.get() == 0;
+}
+
+template<class T> inline bool operator!=(Ref<T> const& p, std::nullptr_t) noexcept
+{
+    return p.get() != 0;
+}
+
+template<class T> inline bool operator!=(std::nullptr_t, Ref<T> const& p) noexcept
+{
+    return p.get() != 0;
+}
+
+template<class T> inline bool operator<(Ref<T> const& a, Ref<T> const& b) noexcept
+{
+    return std::less<T*>()(a.get(), b.get());
+}
+
+template<class T> void swap(Ref<T>& lhs, Ref<T>& rhs) noexcept
+{
+    lhs.swap(rhs);
+}
+
+// pointer casts
+
+template<class T, class U> Ref<T> static_pointer_cast(Ref<U> const& p)
+{
+    return static_cast<T*>(p.get());
+}
+
+template<class T, class U> Ref<T> const_pointer_cast(Ref<U> const& p)
+{
+    return const_cast<T*>(p.get());
+}
+
+template<class T, class U> Ref<T> dynamic_pointer_cast(Ref<U> const& p)
+{
+    return dynamic_cast<T*>(p.get());
+}
+
+
+template<class T, class U> Ref<T> static_pointer_cast(Ref<U>&& p) noexcept
+{
+    return Ref<T>(static_cast<T*>(p.detach()), false);
+}
+
+template<class T, class U> Ref<T> const_pointer_cast(Ref<U>&& p) noexcept
+{
+    return Ref<T>(const_cast<T*>(p.detach()), false);
+}
+
+template<class T, class U> Ref<T> dynamic_pointer_cast(Ref<U>&& p) noexcept
+{
+    T* p2 = dynamic_cast<T*>(p.get());
+
+    Ref<T> r(p2, false);
+
+    if (p2)
+    {
+        p.detach();
     }
 
-    template <class T, class R>
-    bool operator >= (nullptr_t, retain_ptr<T, R> const& rhs) noexcept {
-        return not (nullptr < rhs);
+    return r;
+}
+
+template<class T> struct std::hash<Ref<T>>
+{
+    std::size_t operator()(Ref<T> const& ptr) const
+    {
+        return std::hash<T*>()(ptr.get());
     }
+};
 
-    template <class T, class R>
-    bool operator <= (nullptr_t, retain_ptr<T, R> const& rhs) noexcept {
-        return not (nullptr > rhs);
-    }
 
-    template <class T, class R>
-    bool operator > (nullptr_t, retain_ptr<T, R> const& rhs) noexcept {
-        return not (rhs < nullptr);
-    }
-
-    template <class T, class R>
-    bool operator < (nullptr_t, retain_ptr<T, R> const& rhs) noexcept {
-        return std::less<>()(rhs.get(), nullptr);
-    }
-
-} /* namespace sg14 */
-
-template <class T>
-using RefCountedThreadSafe = sg14::atomic_reference_count<T>;
-template <class T>
-using RefCounted = sg14::reference_count<T>;
-
-template <class T, class R = sg14::retain_traits<T>>
-using Ref = sg14::retain_ptr<T, R>;
+template <class T, class... Types>
+Ref<T> MakeRef(Types&&... args)
+{
+    return Ref<T>(new T(std::forward<Types>(args)...));
+}
 
 #endif // RADCPP_MEMORY_H
